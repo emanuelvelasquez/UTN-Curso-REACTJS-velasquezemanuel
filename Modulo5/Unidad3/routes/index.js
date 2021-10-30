171b84dd90a58d40adab24832aa3227b9cb83043
emanuelvelasquez <emanuel2318@gmail.com>
2021-10-29 23:44:03 -0300

    agrego gitignore

--- Modulo5/Unidad3/routes/index.js
+++ Modulo5/Unidad3/routes/index.js
@@ -1,20 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var pool =require('../db');
+var pool = require("../db");
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-
-  var empleados =[]
-  await pool.query('select * from empleados').then((resultado)=>{
-    empleados= resultado
-  })
-
-  res.render('index', {
-    title: 'Index',
-    empleados:empleados
+router.get("/", async function (req, res, next) {
+  var empleados = [];
+  await pool.query("select * from empleados").then((resultado) => {
+    empleados = resultado;
   });
 
+  res.render("index", {
+    title: "Index",
+    empleados: empleados,
+  });
 });
 
 module.exports = router;
